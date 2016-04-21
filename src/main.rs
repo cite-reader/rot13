006f716b1af7360ea729fa77589420f1159294aa
Alex Hill <alexander.d.hill.89@gmail.com>
2016-04-20 21:22:50 -0700

    Make types a bit more clear

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::ascii::AsciiExt;
-use std::char::from_u32;
+use std::char;
 use std::io::{self, BufRead};
 
 fn main() {
@@ -12,11 +12,11 @@ fn main() {
                  if c.is_ascii() {
                      if c >= 'A' && c <= 'Z' {
                          let r = (c as u32 - 'A' as u32 + 13) % 26;
-                         from_u32(r + 'A' as u32).unwrap()
+                         char::from_u32(r + 'A' as u32).unwrap()
                      }
                      else if c >= 'a' && c <= 'z' {
                          let r = (c as u32 - 'a' as u32 + 13) % 26;
-                         from_u32(r + 'a' as u32).unwrap()
+                         char::from_u32(r + 'a' as u32).unwrap()
                      }
                      else {
                          c
